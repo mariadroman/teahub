0f019efbc61f182e4f7f88cf441396598a225881
mahya <mahyaa.mirtar@gmail.com>
2016-09-02 16:07:47 +0200

    adds compile time dependency injection

---
+++ app/services/impl/ApiTogglService.scala
@@ -0,0 +1,33 @@
+package services.impl
+
+import services.TogglService
+import services.TogglService.Project
+import play.api.libs.ws.WSClient
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.libs.ws.WSAuthScheme.BASIC
+import play.api.libs.json._
+
+/**
+  * Implimatation of [[TogglService]]
+  */
+class ApiTogglService(ws: WSClient)(implicit val ec: ExecutionContext) extends TogglService {
+  /** Request Toggl the list of all projects inside the specified workspace
+    *
+    * @return returns the list of all Toggle projects in the workspace */
+  override def getTogglProjects(apiToken: String): Future[List[String]] = {
+    //TODO: The workspace ID should be passed to this method. Meanwhile, in order to test change the xxxx to a roper workspace.
+    val request = ws.url("https://www.toggl.com/api/v8/workspaces/xxxx/projects")
+      .withHeaders("Content-Type" -> "application/Json")
+      .withAuth(apiToken, "api_token", BASIC)
+
+    request.get().map { response =>
+      val bodyJSValue: JsValue = Json.parse(response.body)
+      val validateBody = bodyJSValue.validate[List[Project]]
+      ws.close()
+      validateBody match {
+        case JsSuccess(projectList: List[Project], _) => projectList.map(_.name)
+        case JsError(_) => List.empty
+      }
+    }
+  }
+}
--- build.sbt
+++ build.sbt
@@ -3,5 +3,16 @@ name := "teahub"
 version := "0.1.0-SNAPSHOT"
 
 scalaVersion := "2.11.8"
+val playVersion = "2.5.4"
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
+lazy val root = (project in file("."))
+  .enablePlugins(PlayScala)
+  .enablePlugins(RoutesCompiler)
+libraryDependencies ++= Seq(
+  jdbc,
+  cache,
+  ws,
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
+)
+routesGenerator := InjectedRoutesGenerator
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
