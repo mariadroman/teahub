ec9319d19ab56299d4bcd0d81f9c09e4940621dc
mahya <mahyaa.mirtar@gmail.com>
2016-09-09 17:54:07 +0200

    Changes for routes

--- app/controllers/TEAHubController.scala
+++ app/controllers/TEAHubController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.libs.json.Json
 import play.api.mvc.Action
 import play.api.mvc._
 import services.TogglService
@@ -10,11 +11,17 @@ import scala.concurrent.{ExecutionContext, Future}
   * @param togglService Service that requests Toggl the list of the projects */
 class TEAHubController(togglService: TogglService)(implicit executionContext: ExecutionContext) extends Controller {
   /** Request List of Toggl projects' name from Toggl API and show it in TEAHub UI
-    *
-    * @param apiToken is a unique String that identifies the Toggl users. The value can be find under My Profile in Toggl account */
+    */
   //TODO: So far this method just takes the projects name in future it should show the result in the related page in TEAHUB
-  def togglProjects(apiToken: String) = Action.async { implicit request =>
-    val result: Future[List[String]] = togglService.getTogglProjects(apiToken)
-    Future.successful(Ok("Hello Toggl!"))
+  def togglProjects = Action.async { implicit request =>
+    val toggleToken: Option[String] = request.getQueryString("apiToken")
+    val result = toggleToken match {
+      case Some(token) => togglService.getTogglProjects(token)
+      case None => Future.successful(List.empty)
+    }
+    result.map {
+      theResult =>
+        Ok(Json.obj("Projects" -> theResult))
+    }
   }
 }
