e0af4b99eee8bf09dc8e033eb70b4752fe284547
José Cerón <jose.ceron@lunatech.com>
2017-01-23 13:30:17 +0100

    The togglToken is now retrieved from the form

--- app/controllers/UIController.scala
+++ app/controllers/UIController.scala
@@ -16,11 +16,17 @@ class UIController(val messagesApi: MessagesApi)(implicit executionContext: Exec
   val togglTokenForm = Form(single("togglToken" -> text()))
   val projectName = Form(single("projectName" -> text()))
 
-  val emptyList = List("aa","bb")
-  //val testingList = ApiTogglService.
-
   def management = Action { implicit request => Ok(views.html.user_management()) }
-  def list = Action { implicit request => Ok(views.html.projects(emptyList)) }
+  def list = Action { implicit request => {
+      togglTokenForm.bindFromRequest().fold(
+        error => BadRequest, // Warning here because 'error' is never used
+        data => {
+          System.out.println("JOSE: the togglToken is " + data) // TODO: togglToken should be put inside the cache
+          Ok(views.html.projects())
+        }
+      )
+    }
+  }
   def setup = Action { implicit request => Ok(views.html.setup_projects(togglTokenForm)) }
   def details = Action { implicit request => Ok(views.html.project_details()) }
   def newProject = Action { implicit request => Ok(views.html.new_project(projectName)) }
--- app/services/impl/ApiTogglService.scala
+++ app/services/impl/ApiTogglService.scala
@@ -2,11 +2,12 @@ package services.impl
 
 import services.TogglService
 import services.TogglService.{Project, Workspace}
+
 import play.api.libs.ws.WSClient
-import scala.concurrent.{Await, ExecutionContext, Future}
 import play.api.libs.ws.WSAuthScheme.BASIC
 import play.api.libs.json._
 
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
 
 /**
@@ -22,20 +23,13 @@ class ApiTogglService(ws: WSClient)(implicit val ec: ExecutionContext) extends T
     * @return the list of all Toggle projects in the workspace
     */
   override def getTogglProjects(apiToken: String): Future[List[String]] = {
-    //TODO: The workspace ID should be passed to this method.
-    // Meanwhile, in order to test change the xxxx to a roper workspace.
+    // Getting the workspace ID
+    val workspaceFuture = getTogglWorkspace(apiToken)
+    val duration = Duration(2, SECONDS) // TODO: I don't like the way this is implemented. Needs review
+    val workspaceNoFuture = Await.result(workspaceFuture, duration)
+    val workspaceID = workspaceNoFuture.head
 
-    val workspace = getTogglWorkspace(apiToken)
-    System.out.println("JOSE: workspace "+workspace)
-    // for {res <- workspace } yield res.foreach(println)
-    val duration = Duration(2, SECONDS)
-    val myResult  = Await.result(workspace,duration)
-    System.out.println("JOSE: before the big thing")
-    val test=myResult.head
-    myResult.foreach(println)
-    System.out.println("JOSE: and the head "+test)
-
-    val request = ws.url(s"https://www.toggl.com/api/v8/workspaces/"+test+"/projects")
+    val request = ws.url(s"https://www.toggl.com/api/v8/workspaces/" + workspaceID + "/projects")
       .withHeaders("Content-Type" -> "application/Json")
       .withAuth(apiToken, "api_token", BASIC)
 
@@ -51,27 +45,19 @@ class ApiTogglService(ws: WSClient)(implicit val ec: ExecutionContext) extends T
   }
 
   override def getTogglWorkspace(apiToken: String): Future[List[Long]] = {
-    println("el primero - " + apiToken)
     val request = ws.url("https://www.toggl.com/api/v8/workspaces")
       .withHeaders("Content-Type" -> "application/Json")
       .withAuth(apiToken, "api_token", BASIC)
 
-    println("el segundo - " + request.get())
-
     request.get().map { response =>
-      println(s"Dentro response: ")
-      println(s"${response.body}")
       val bodyJSValue: JsValue = Json.parse(response.body)
       val validateBody = bodyJSValue.validate[List[Workspace]]
       validateBody match {
         case JsSuccess(workspaceList: List[Workspace], _) =>
-          println("Ben")
           workspaceList.map(_.id)
         case JsError(_) =>
-          println("Bad")
           List.empty
         case _ =>
-          println("Really bad")
           List.empty
       }
     }
